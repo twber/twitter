8c8f78c70bd5508c988a1911013350fbd383a664
twber <hctsai@umich.edu>
2020-08-03 21:19:49 +0800

    Initial Commit

---
+++ twitter/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  twitter
+//
+//  Created by Bernice TSAI on 2020/8/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
